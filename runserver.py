db8cbfe622ba58e430a3707e198c5b7cdb7f32a4
yhf <xdhfying@gmail.com>
2015-04-30 23:36:53 +0800

    second commit

--- runserver.py
+++ runserver.py
@@ -8,4 +8,4 @@
 from mysite import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
--- views/index.py
+++ views/index.py
@@ -3,17 +3,29 @@
 # @Author: yhf
 # @Date:   2015-04-13 18:53:53
 # @Last Modified by:   yhf
-# @Last Modified time: 2015-04-14 22:59:01
+# @Last Modified time: 2015-04-30 23:04:39
 
 from flask import render_template
 from mysite import app
 from mysite.models.article import Article
 
 
+NUM_ARTICLES_PER_PAGE = 50
+
+
 @app.route("/")
-def hello():
-    articles = Article.query.with_entities(Article.id, Article.title, Article.time).limit(10).all()
-    return render_template('index.html', articles=articles)
+def index():
+    articles = Article.query.with_entities(Article.id, Article.title, Article.time).limit(NUM_ARTICLES_PER_PAGE).all()
+    return render_template('index.html', articles=articles, page_num=1)
+
+
+@app.route("/page/<int:page_num>")
+def page(page_num):
+    offset = (page_num - 1) * NUM_ARTICLES_PER_PAGE
+    articles = Article.query.with_entities(Article.id, Article.title, Article.time).offset(offset).limit(NUM_ARTICLES_PER_PAGE)
+    if not len(list(articles)):
+        return render_template('404.html'), 404
+    return render_template('index.html', articles=articles, page_num=page_num)
 
 
 @app.route("/article/<int:article_id>", methods=['GET'])
